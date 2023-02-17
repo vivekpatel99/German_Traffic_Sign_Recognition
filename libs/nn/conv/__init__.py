239b2cd4f1d36778d8f158aa0c8e84b351ac83c2
vivekpatel99 <vivpatel99@gmail.com>
2023-02-17 19:32:37 +0100

    project completed

--- libs/nn/conv/__init__.py
+++ libs/nn/conv/__init__.py
@@ -1,3 +1,3 @@
 
-from .cnn import CNN
+from .lenet import LeNet
 
---
+++ libs/nn/conv/lenet.py
@@ -0,0 +1,34 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import Dense
+from tensorflow.keras import backend as K
+
+
+class LeNet:
+    @staticmethod
+    def build(width:int, height:int, depth:int, classes:int):
+        inputShape = (height, width, depth)
+
+        # if we are using "channels first", update the input shape
+        if K.image_data_format() == "channels_first":
+            inputShape = (depth, height, width)
+
+        # initialize the model
+        return Sequential([
+
+            # first set of CONV => RELU => POOL layers
+            Conv2D(20, (5, 5), padding='same',  activation='relu',input_shape=inputShape),
+            MaxPooling2D(pool_size=(2, 2), strides=(2, 2)),
+
+            # second set of CONV => RELU => POOL layers
+            Conv2D(50, (5, 5), padding='same', activation='relu'),
+            MaxPooling2D(pool_size=(2, 2), strides=(2, 2)),
+
+            Flatten(),
+            Dense(500, activation='relu'),
+            Dense(classes, activation='softmax'),
+        ])
